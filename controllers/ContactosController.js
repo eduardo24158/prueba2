550b1028c335e104cf20ec57872bb04d6669c3ab
eduardo24158 <hurtadoeduardo227@gmail.com>
2025-01-04 16:21:58 -0400

    hola6

--- controllers/ContactosController.js
+++ controllers/ContactosController.js
@@ -22,7 +22,7 @@ class ContactosController {
     const verificationURL = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`;
     
     
-    const ipapURL=`http://ip-api.com/json/?fields=61439`;
+    const ipapURL=`http://ip-api.com/json/${clientIp}`;
     axios
     .get(ipapURL)
     .then((response) => {
